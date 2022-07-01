004bc2f9204bb87cfee88fae62b6b15c23ff5720
Bruno Palma <b.palma@accenture.com>
2022-07-01 15:39:04 +0100

    changes connectDB

--- public/js/connectDB.js
+++ public/js/connectDB.js
@@ -1,12 +1,25 @@
 const { Client } = require("pg");
+
 const client = new Client({
   user: "postgres",
-  host: "89.114.30.7",
+  host: "localhost",
   database: "postgres",
   password: "postgres",
   port: 5432,
 });
+
 client.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
+  if (!err) {
+    console.log("Connected!");
+  } else {
+    console.log(ererr.messager);
+  }
+});
+
+client.query("SELECT * FROM configparameter", (err, res) => {
+  if (!err) {
+    console.log(res.rows);
+  } else {
+    console.log(err.message);
+  }
 });
