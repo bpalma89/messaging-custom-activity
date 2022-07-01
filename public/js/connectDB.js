00ab4aac7bf615efb8cf1283f8d323e84ff262ae
Bruno Palma <b.palma@accenture.com>
2022-07-01 14:51:43 +0100

    dependencies updated

--- public/js/connectDB.js
+++ public/js/connectDB.js
@@ -1,14 +1,12 @@
-"use strict";
-const { Client } = require('pg')
+const { Client } = require("pg");
 const client = new Client({
-  user: 'postgres',
-  host: '89.114.30.7',
-  database: 'postgres',
-  password: 'postgres',
+  user: "postgres",
+  host: "89.114.30.7",
+  database: "postgres",
+  password: "postgres",
   port: 5432,
-})
-client.connect(function(err) {
+});
+client.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
-
